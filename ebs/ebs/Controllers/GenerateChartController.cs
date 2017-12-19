a1044656a9394c34996e4bafbe88104ab0774ae1
Belma Homarac <belmah95@outlook.com>
2017-12-19 20:51:26 +0100

    Simple page for generating charts added

---
+++ ebs/ebs/Controllers/GenerateChartController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ebs.Controllers
+{
+    public class GenerateChartController : Controller
+    {
+        // GET: GenerateChart
+        public ActionResult GenerateChart()
+        {
+            return View();
+        }
+    }
+}
