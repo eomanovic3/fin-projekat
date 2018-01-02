a7f6df2e978f2e0702560c70f7751af938a1e3b8
minaLala123 <eminaomanovic123@gmail.com>
2018-01-02 15:50:44 +0100

    GenerateChart.cshtml is working now

---
+++ ebs/ebs/Models/Chart.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ebs.Models
+{
+    public class Chart
+    {
+        public List<string> labels { get; set; }
+        public List<Datasets> datasets { get; set; }
+    }
+    public class Datasets
+    {
+        public string label { get; set; }
+        public List<string> backgroundColor { get; set; }
+        public List<string> borderColor { get; set; }
+        public List<string> borderWidth { get; set; }
+        public List<float> data { get; set; }
+    }
+}
